a4b3db93cc640f029d2fef0ed4df6bcc9f012321
Vineet Gupta <vgupta@synopsys.com>
2014-10-07 16:52:21 +0530

    ARC_SUPPORT can't be used simply
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- testcases/kernel/syscalls/sync_file_range/sync_file_range01.c
+++ testcases/kernel/syscalls/sync_file_range/sync_file_range01.c
@@ -216,7 +216,7 @@ static inline long syncfilerange(int fd, off64_t offset, off64_t nbytes,
 #else
 	return ltp_syscall(__NR_sync_file_range2, fd, flags, offset, nbytes);
 #endif
-#elif defined(ARC_SUPPORT)
+#elif defined(__arc__)  //ARC_SUPPORT == 1
 	return sync_file_range(fd, offset, nbytes, flags);
 #else
 	return ltp_syscall(__NR_sync_file_range, fd, offset, nbytes, flags);
