c0dbed7cf6804f12f9a32654c704e3b62859e13c
Vineet Gupta <vgupta@synopsys.com>
2015-06-28 21:20:50 +0530

    Fix sigaction tests for ARC
    ARC signal handling relies on a working SA_RESTORER (default provided
    for by uClibc) when calling sigaction().
    
    LTP OTOH uses a generic syscall() based sigaction wrapper which fails to
    provide a restorer causing sigreturn to weer off to la la land, causing
    the segfaults, hence test fail reports.
    
    So make LTP use uClibc:sigaction() wrapper for ARC.
    
    This shd fix rt_sigaction{01,02,03}, rt_sig{procmask01,suspend01}
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- include/ltp_signal.h
+++ include/ltp_signal.h
@@ -53,4 +53,13 @@
 #define SIGSETSIZE (_NSIG / 8)
 #endif
 
+#ifdef __ARC__
+#define ltp_syscall_sigaction(NR, sig, act, oat, ...) ({ \
+	__ret = sigaction(sig, act, oact);		\
+	__ret; \
+})
+#else
+#define ltp_syscall_sigaction	ltp_syscall
+#endif
+
 #endif
